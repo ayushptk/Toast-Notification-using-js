106ff95821bcc3aa9ba9e88b4c36f52f1fe1d1fb
ayushptk <ptkaayush13@gmail.com>
2024-07-06 13:01:29 +0545

    Update Script

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
  let toastBox = document.getElementById('toastBox');
- function showtoast(){
+ let imgbox = document.getElementById('imgbox');
+ function Showtoast(){
      let toast = document.createElement('div');
      toast.classList.add('toast');
      toast.innerHTML = 'You have successfully logged in!';
+     toastBox.appendChild(toast);
 
  }
+//  function ShowImage(){
+//     let img = document.createElement('img');
+//     img.classList.add('img');
+//     img.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwGxx1DUJNuxRai0IV8G04TKWCpkcmRlkRhA&s';
+//     imgbox.appendChild(img);
+// }
