3b76fde389c8cd95d7e27784c5d4ade866cfe935
ayushptk <ptkaayush13@gmail.com>
2024-07-10 11:33:46 +0545

    update

--- script.js
+++ script.js
@@ -10,6 +10,14 @@
      toast.classList.add('toast');
      toast.innerHTML = msg;
      toastBox.appendChild(toast);
+
+     if(msg.includes('error')){
+        toast.classList.add('error');
+     }
+     if(msg.includes('Invalid')){
+        toast.classList.add('invalid');
+     }
+     
  }
 //  function ShowImage(){
 //     let img = document.createElement('img');
